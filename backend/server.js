0f26ec3324cf5caeb769b451716ddc36c90206b2
Mahi Sawner <mahisawner1234@gmail.com>
2026-03-25 10:00:00 +0530

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
 const githubAuthRouter = require('./OAuth/Auth.Github.js');
 
 dotenv.config()
 const app = express()
+
+// Connect to Database
+connectDB();
+
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/OAuth', githubAuthRouter)
 
-const PORT = 5001
+const PORT = process.env.PORT || 5001
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
